e0993634c0a4099d713cb84fa645cd327434e402
j51217123 <65345958+j51217123@users.noreply.github.com>
2022-06-30 18:06:43 +0800

    create colorblock shadow

--- src/components/Color/ColorPalette.Styled.jsx
+++ src/components/Color/ColorPalette.Styled.jsx
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
+export const StyledH1 = styled.h1`
+    margin-bottom: 10px;
+`
+
+export const StyledColorBlockWrapper = styled.div`
+    border-radius:10px;
+    overflow: hidden;
+    box-shadow: rgba(17, 22, 26, 0.16) 0px 4px 8px 0px;
+`
+
 export const StyledColorPaletteWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
@@ -9,6 +19,8 @@ export const StyledColorPaletteContainer = styled.div`
     flex-direction: column;
     padding: 10px;
     text-align: center;
+    overflow: hidden;
+    border-radius: 10px;
 `;
 
 export const StyledColorBlock = styled.div`

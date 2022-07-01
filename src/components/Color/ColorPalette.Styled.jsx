56c89ae3b47aed3ed55755959ecdcb7badff9f3d
j51217123 <65345958+j51217123@users.noreply.github.com>
2022-07-01 18:32:39 +0800

    modified props name

--- src/components/Color/ColorPalette.Styled.jsx
+++ src/components/Color/ColorPalette.Styled.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const StyledH1 = styled.h1`
-    margin-bottom: 10px;
-`
+    padding-bottom: 10px;
+`;
 
 export const StyledColorBlockWrapper = styled.div`
-    border-radius:10px;
+    border-radius: 10px;
     overflow: hidden;
     box-shadow: rgba(17, 22, 26, 0.16) 0px 4px 8px 0px;
-`
+`;
 
 export const StyledColorPaletteWrapper = styled.div`
     display: flex;
--- src/components/Icon/Icons.Styled.jsx
+++ src/components/Icon/Icons.Styled.jsx
@@ -1,20 +1,5 @@
 import styled, { css } from "styled-components";
-// import { ReactSVG } from "react-svg";
 import SVG from "react-inlinesvg";
-
-// export const StyledIcon = styled(SVG)`
-//     line-height: 1;
-//     margin-left: ${({ iconPosition }) => (iconPosition === "end" ? 8 : 0) + "px"};
-//     margin-right: ${({ iconPosition }) => (iconPosition === "start" ? 8 : 0) + "px"};
-
-//     svg {
-//         width: ${({ size }) => `${size}`};
-//         height: ${({ size }) => `${size}`};
-//         fill: ${({ theme }) => {
-//             console.log("check theme:", theme);
-//         }};
-//     }
-// `;
 
 export const StyledIcon = styled(SVG)`
     ${({ theme, color, iconColor }) => {
@@ -22,7 +7,7 @@ export const StyledIcon = styled(SVG)`
         return css`
             line-height: 1;
             margin-left: ${({ iconPosition }) => (iconPosition === "end" ? 8 : 0) + "px"};
-            margin-right: ${({ iconPosition }) => iconPosition === "start" && 8 + "px"};
+            margin-right: ${({ iconPosition }) => (iconPosition === "end" ? 0 : 8) + "px"};
 
             svg {
                 width: ${({ size }) => size};
@@ -32,41 +17,3 @@ export const StyledIcon = styled(SVG)`
         `;
     }}
 `;
-
-// export const StyledIcon = styled(ReactSVG)`
-//     line-height: 1;
-
-//     svg {
-//         width: ${({ size }) => `${size}`};
-//         height: ${({ size }) => `${size}`};
-//         /* fill: ${({ iconColor, theme }) => (iconColor ? iconColor : `${theme.colors.blue700}`)}; */
-//         /* fill: ${({ iconColor, theme }) => {
-//             console.log(theme, "theme");
-//             return iconColor ? iconColor : `${theme.colors.blue700}`;
-//         }}; */
-//         fill: ${({ isDisabled, iconColor, theme }) => {
-//             console.log(theme, "theme");
-//             return isDisabled ? `red` : iconColor;
-//         }};
-
-//         ${({ size }) =>
-//             size &&
-//             css`
-//                 width: ${size};
-//                 height: ${size};
-//             `}
-//         ${({ transform }) =>
-//             transform &&
-//             css`
-//                 transform: ${transform};
-//             `}
-//     path {
-//             ${({ color }) =>
-//                 color &&
-//                 css`
-//                     fill: ${({ color, theme }) => (color ? color : `${theme.colors.blue700}`)};
-//                     /* fill: ${({ color, theme }) => (color ? color : `red`)}; */
-//                 `}
-//         }
-//     }
-// `;

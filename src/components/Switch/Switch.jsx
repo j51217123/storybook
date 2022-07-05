bda0d8107424afbcb884dc3a23ca49303c4d83e8
j51217123 <65345958+j51217123@users.noreply.github.com>
2022-07-05 14:21:49 +0800

    create switch component

---
+++ src/components/Switch/Switch.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import ButtonBase from "../ButtonBase/ButtonBase";
+
+const Switch = props => {
+    return (
+        <>
+            <div>111</div>
+        </>
+    );
+};
+
+export default Switch;

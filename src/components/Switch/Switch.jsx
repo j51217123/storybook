aa37ae1dbe0947ca8d7ace203fcb25e2cffdef72
j51217123 <65345958+j51217123@users.noreply.github.com>
2022-07-06 18:04:54 +0800

    create size feature

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { StyledSwitchWrapper, StyledSlider, StyledSwitch } from "./Switch.Styled";
 
 const Switch = props => {
+    const { backgroundColor, size } = props;
     const [toggled, setToggled] = useState(false);
 
     const onChange = e => {
@@ -11,12 +12,22 @@ const Switch = props => {
 
     return (
         <>
-            <StyledSwitchWrapper>
-                <StyledSwitch onChange={onChange} />
-                <StyledSlider className="slider" />
+            <StyledSwitchWrapper >
+                <StyledSwitch className="StyledSwitch" onChange={onChange} backgroundColor={backgroundColor} />
+                <StyledSlider className="StyledSlider" size={size}/>
             </StyledSwitchWrapper>
         </>
     );
+};
+
+Switch.propTypes = {
+    className: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    /**
+     * The size of the component.
+     * @default 'medium'
+     */
+    size: PropTypes.oneOf(["small", "medium", "large"], PropTypes.string),
 };
 
 export default Switch;
--- src/stories/Switch.stories.jsx
+++ src/stories/Switch.stories.jsx
@@ -1,17 +1,28 @@
+import { lightBlue } from "@mui/material/colors";
 import Switch from "../components/Switch/Switch";
 
 export default {
     title: "Components/Switch/Switch",
     component: Switch,
+    argTypes: {
+        backgroundColor: { control: "color" },
+        size: {
+            options: ["small", "medium", "large"],
+            control: { type: "radio" },
+        },
+    },
 };
 
 // const Template = args => <Button {...args} />;
 const Template = args => {
     return (
         <>
-            <Switch />
+            <Switch {...args} />
         </>
     );
 };
 
 export const Default = Template.bind({});
+Default.args = {
+    backgroundColor: "red",
+};
